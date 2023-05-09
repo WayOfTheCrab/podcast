0d8ae00088340889f3bb53c3b2beace1aabefa26
Jonathan Johnson <jon@khonsulabs.com>
2023-05-09 08:39:29 -0700

    Fixing assignment to const
    Didn't notice the assignment in the callback when simplifying the code.

--- static/player.js
+++ static/player.js
@@ -5,10 +5,10 @@ onload = (_) => {
         const player = players[i];
         const id = "player-id-" + player.currentSrc;
 
-        const lastTime = parseFloat(window.localStorage.getItem(id)) || 0;
+        var lastTime = parseFloat(window.localStorage.getItem(id)) || 0;
 
         player.currentTime = lastTime;
-        player.ontimeupdate = (event) => {
+        player.ontimeupdate = (_) => {
             if (Math.abs(player.currentTime - lastTime) > 5) {
                 lastTime = player.currentTime;
                 window.localStorage.setItem(id, lastTime);
