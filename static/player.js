cf934fbb45a027e58c2dadf08b3d90aea2801abe
Togglebit <me@togglebit.io>
2023-06-25 12:19:35 +0200

    make it possible to link to a particular playhead location

--- static/player.js
+++ static/player.js
@@ -1,18 +1,51 @@
+const copyPosToClipboard = (ev) => {
+    ev.preventDefault();
+
+    const permaLink = document.querySelector(".perma-link");
+    navigator.clipboard.writeText(permaLink.href);
+
+    const origText = permaLink.innerText;
+    permaLink.innerText = "Copied to clipboard...";
+    ev.target.onmouseleave = () => {
+        setTimeout(() => {
+            permaLink.innerText = origText;
+        }, 1000);
+    };
+}
+
 onload = (_) => {
-    const players = document.querySelectorAll("audio");
+    // -----------------------------------------------------------------------------
+    //     - Link to a position -
+    // -----------------------------------------------------------------------------
+    const querystring = new URLSearchParams(window.location.search);
+    const playhead = querystring.get("t");
 
-    for (i in players) {
-        const player = players[i];
-        const id = "player-id-" + player.currentSrc;
+    // -----------------------------------------------------------------------------
+    //     - Remember playhead position -
+    // -----------------------------------------------------------------------------
+    const player = document.querySelector("audio");
+    const permaLink = document.querySelector(".perma-link");
+    if (!(player || permaLink)) return;
 
-        var lastTime = parseFloat(window.localStorage.getItem(id)) || 0;
+    permaLink.onclick = copyPosToClipboard;
 
-        player.currentTime = lastTime;
-        player.ontimeupdate = (_) => {
-            if (Math.abs(player.currentTime - lastTime) > 5) {
-                lastTime = player.currentTime;
-                window.localStorage.setItem(id, lastTime);
-            }
-        };
+    const id = "player-id-" + player.currentSrc;
+
+    var lastTime = parseFloat(window.localStorage.getItem(id)) || 0;
+
+    player.currentTime = lastTime;
+    player.ontimeupdate = (_) => {
+        permaLink.href = `${permaLink.dataset['url']}?t=${Math.round(player.currentTime)}`;
+        if (Math.abs(player.currentTime - lastTime) > 5) {
+            lastTime = player.currentTime;
+            window.localStorage.setItem(id, lastTime);
+        }
+    };
+
+    // If the playhead position is set in the querystring
+    // that will take precedence over the stored location
+    if (playhead != null) {
+        const playheadPos = parseFloat(playhead || 0);
+        player.currentTime = playheadPos;
     }
 };
